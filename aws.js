26739d4390d0ead33a1f6cb625b2c47a9caff966
mehdibh <mehdibhaddou@gmail.com>
2023-05-04 17:10:18 +0200

    Init project

---
+++ aws.js
@@ -0,0 +1,8 @@
+const AWS = require('aws-sdk');
+const dotenv = require('dotenv');
+
+AWS.config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
